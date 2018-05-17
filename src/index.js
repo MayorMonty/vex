5f2b5f8ad5b59cf77197a9f0c4298600786b37b4
MayorMonty <artificalchamber@gmail.com>
2018-05-16 23:39:50 -0400

    Move app.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 import "./style";
-import App from "./components/app";
+import { h, Component } from "preact";
+import { Router } from "preact-router";
 
-export default App;
+import Header from "./components/header";
+import Home from "async!./routes/home";
+import Event from "async!./routes/event";
+
+if (module.hot) {
+  require("preact/debug");
+}
+
+export default class App extends Component {
+  /** Gets fired when the route changes.
+   *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
+   *	@param {string} event.url	The newly routed URL
+   */
+  handleRoute = e => {
+    this.currentUrl = e.url;
+  };
+
+  render() {
+    return (
+      <div id="app">
+        <Header />
+        <Router onChange={this.handleRoute}>
+          <Home path="/" />
+          <Event path="/event/:sku" />
+        </Router>
+      </div>
+    );
+  }
+}
