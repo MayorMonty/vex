e2097bb592005b1e2499dcec7f795e0e1ec1d097
MayorMonty <artificalchamber@gmail.com>
2018-05-18 00:33:17 -0400

    Rankings

--- src/routes/event/Rankings/index.js
+++ src/routes/event/Rankings/index.js
@@ -1,17 +1,45 @@
 import { h, Component } from "preact";
 import Load from "../../../components/load";
+import Team from "../../../components/team";
 import vexdb from "vexdb";
 
-export default class Rankings extends Component {
-  render({ division, sku }) {
-    console.log(division, sku);
-    if (!division) return undefined;
-    return (
-      <Load content={{ ranks: vexdb.get("rankings", { sku, division }) }}>
-        {({ ranks }) => (
-          <ol>{ranks.map(rank => <li key={rank.team}>{rank.team}</li>)}</ol>
-        )}
-      </Load>
-    );
-  }
-}
+import List from "preact-material-components/List";
+import "preact-material-components/List/style.css";
+import Checkbox from "preact-material-components/Checkbox";
+import "preact-material-components/Checkbox/style.css";
+
+export default ({ division, sku }) =>
+  division ? (
+    <Load
+      content={{
+        ranks: vexdb
+          .get("rankings", {
+            sku,
+            division
+          })
+          .then(ranks =>
+            Promise.all(
+              ranks.map(async rank => ({
+                ...rank,
+                team: await vexdb
+                  .get("teams", { team: rank.team })
+                  .then(t => t[0])
+              }))
+            )
+          )
+      }}
+    >
+      {({ ranks }) => (
+        <List compact>
+          <List.Item>
+            <List.ItemGraphic>
+              <Checkbox />
+            </List.ItemGraphic>
+          </List.Item>
+          {ranks.map(rank => <Team team={rank.team} />)}
+        </List>
+      )}
+    </Load>
+  ) : (
+    undefined
+  );
